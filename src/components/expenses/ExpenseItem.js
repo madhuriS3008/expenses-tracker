704ac3b901b3d929a10c58a6638b6f1505cab8de
Madhuri Sabbineni <madhusabbineni@gmail.com>
2021-12-30 22:38:26 -0600

    displayed expenses for the tracker

---
+++ src/components/expenses/ExpenseItem.js
@@ -0,0 +1,18 @@
+import classes from "./ExpenseItem.module.css";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../ui/Card";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className={classes["expense-item"]}>
+      <ExpenseDate date={props.date} />
+      <div className={classes["expense-item__description"]}>
+        <h2>{props.title}</h2>
+        <div className={classes["expense-item__price"]}>${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
