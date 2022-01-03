96f0d4887d56f829604eee86ee398a88bf39224c
Madhuri Sabbineni <madhusabbineni@gmail.com>
2022-01-03 14:25:40 -0600

    Conditional rendering lists, added chart component

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -24,11 +24,12 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
     props.onSaveExpenseData(expenseData);
+
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
@@ -67,6 +68,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className={classes["new-expense__actions"]}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import classes from "./NewExpense.module.css";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: "e" + (Math.random() * 10).toString(),
     };
     props.onAddExpense(expenseData);
+    toggleForm();
+  };
+
+  const toggleForm = () => {
+    setShowForm((prevShowForm) => !prevShowForm);
   };
 
   return (
     <div className={classes["new-expense"]}>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!showForm && <button onClick={toggleForm}>Add New Expense</button>}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={toggleForm}
+        />
+      )}
     </div>
   );
 };
