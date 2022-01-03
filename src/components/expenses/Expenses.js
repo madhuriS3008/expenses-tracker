17a62b86633f1f9b21750f7f3357ff26d7f6ce39
Madhuri Sabbineni <madhusabbineni@gmail.com>
2022-01-03 10:31:38 -0600

    Added expenses filter based on year

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import classes from "./Expenses.module.css";
 
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../ui/Card";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className={classes.expenses}>
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
